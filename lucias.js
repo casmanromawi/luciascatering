b5628e456170e98ea4c0dce7a7e1b7803482ba2a
casmanromawi <91203805+casmanromawi@users.noreply.github.com>
2021-09-23 15:28:22 +0700

    Update lucias.js

--- lucias.js
+++ lucias.js
@@ -19,13 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="app-inventor.html"}
 if(x=="2"){xxx="google-reverse-image-search.html"}
 if(x=="3"){xxx="sound-text-google.html"}
 if(x=="4"){xxx="google.html"}
-if(x=="5"){xxx="amazon.html"}a_to_ve[i].href="http://www.luciascatering.com/2021/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="amazon.html"}
+if(x=="6"){xxx="marketplace.html"}a_to_ve[i].href="http://www.luciascatering.com/2021/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
